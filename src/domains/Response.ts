896e4fb1afb6de47b3a9788a5a2708d36cfc3597
Alpredo Pandiangan <alpredo.vandy@gmail.com>
2025-06-11 03:54:50 +0700

    domains: add domain response and github

--- src/domains/Response.ts
+++ src/domains/Response.ts
@@ -1,22 +1,14 @@
 import { z } from 'zod';
 
-export const metaSchema = z.object({
-    page: z.union([z.number(), z.string()]),
-    per_page: z.union([z.number(), z.string()]),
-    total_page: z.number(),
-    total_data: z.number(),
-});
-export type MetaType = z.infer<typeof metaSchema>;
-
 export const paginateResponseSchema = <T extends z.ZodTypeAny>(contentSchema: T) =>
     z.object({
         data: contentSchema.optional(),
-        meta: metaSchema.optional(),
     });
 export type PaginateResponseType<T = unknown> = z.infer<ReturnType<typeof paginateResponseSchema<z.ZodType<T>>>>;
 
 export const paginateRequestSchema = z.object({
     type: z.string().optional(),
+    page: z.number().optional(),
     limit: z.number().optional(),
     search: z.string().optional(),
 });
